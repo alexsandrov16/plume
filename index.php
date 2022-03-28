994655227ca2dc87c2413787df0ec6c7329b05dd
alexsandrov16 <alejandro920131@gmail.com>
2022-03-28 15:56:32 -0400

    Create autoload function PSR4

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @package Plume
+ */
+
+use Plume\Kernel\App;
+
+define('PLUME', true);
+
+//Constantes iniciales
+defined('DS') || define('DS', DIRECTORY_SEPARATOR);
+defined('ABS_PATH') || define('ABS_PATH', __DIR__ . DS);
+
+//Autocargador
+require 'src/autoload.php';
+
+$app = new App;
---
+++ src/Framework/App.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Plume\Kernel;
+
+/**
+ * undocumented class
+ */
+class App
+{
+    public function __construct()
+    {
+        echo __CLASS__;
+    }
+}
