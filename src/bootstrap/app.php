7ac434de1a00341b121fa12f3091ed7e0a69b496
alexsandrov16 <alejandro920131@gmail.com>
2022-03-29 23:16:17 +0000

    Rename file

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -2,8 +2,15 @@
 defined('PLUME') || die;
 
 /**
+ * Botstrap app
+ * 
+ * Este proceso configura las constantes de ruta, carga nuestro
+ * cargador automático, junto con el de Composer, carga nuestras funciones 
+ * y activa un arranque específico del entorno.
+ * 
  * @package
  */
+
 
 //Defines
 require_once 'define.php';
@@ -14,7 +21,7 @@ require_once 'autoload.php';
 //Functions 
 require_once 'functions.php';
 
-//Installing CMS
-if (file_exists(ABS_PATH.'installphp')) {
+//Installing CMS esto ba en el fichero index
+if (file_exists(ABS_PATH . 'installphp')) {
     require 'install.php';
 }
