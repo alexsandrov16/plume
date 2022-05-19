a0fc16d56085728278297d21541873f92ebb7bfc
alexsandrov16 <alejandro920131@gmail.com>
2022-05-19 12:05:33 -0400

    yea

---
+++ srcg/bootstrap/app.php
@@ -0,0 +1,27 @@
+<?php
+defined('PLUME') || die;
+
+/**
+ * Botstrap app
+ * 
+ * Este proceso configura las constantes de ruta, carga nuestro
+ * cargador automático, junto con el de Composer, carga nuestras funciones 
+ * y activa un arranque específico del entorno.
+ * 
+ * @package
+ */
+
+
+//Defines
+require_once 'defines.php';
+
+//Autoloader PSR4
+require_once 'autoload.php';
+
+//Functions 
+require_once ABS_PATH . 'src/framework/functions.php';
+
+//Installing CMS esto ba en el fichero index
+if (file_exists(ABS_PATH . 'installphp')) {
+    require 'install.php';
+}
---
+++ srcg/bootstrap/defines.php
@@ -0,0 +1,28 @@
+<?php
+defined('PLUME') || die;
+
+/**
+ * @package Plume
+ */
+/*
+//Flatpage
+defined('FP_CORE') || define('FP_CORE', FP_PATH . 'core' . DS);
+defined('FP_SRC') || define('FP_SRC', FP_PATH . 'src' . DS);
+//Content
+defined('FP_CFG') || define('FP_CFG', ABS_PATH . 'contents' . DS . 'config' . DS);
+defined('FP_PAGES') || define('FP_PAGES', ABS_PATH . 'contents' . DS . 'pages' . DS);
+defined('FP_LOGS') || define('FP_LOGS', ABS_PATH . 'contents' . DS . 'logs' . DS);
+defined('FP_BACKUPS') || define('FP_BACKUPS', ABS_PATH . 'contents' . DS . 'backups' . DS);
+defined('FP_UPLOAD') || define('FP_UPLOAD', ABS_PATH . 'contents' . DS . 'upload' . DS);
+//Themes
+defined('FP_THEMES') || define('FP_THEMES', ABS_PATH . 'template' . DS);
+*/
+
+//bin
+defined('PATH_BIN') || define('PATH_BIN', ABS_PATH . 'bin');
+
+//contents
+defined('PATH_CFG') || define('PATH_CFG', ABS_PATH . 'content' . DS . 'config' . DS);
+defined('PATH_PAGES') || define('PATH_PAGES', ABS_PATH . 'content' . DS . 'pages' . DS);
+defined('PATH_LOGS') || define('PATH_LOGS', ABS_PATH . 'content' . DS . 'logs' . DS);
+defined('PATH_UPLOAD') || define('PATH_UPLOAD', ABS_PATH . 'content' . DS . 'upload' . DS);
