5f2869b0b3cb7c08fb483e37cc10eee1f2aaae0a
alexsandrov16 <alejandro920131@gmail.com>
2022-05-31 21:42:14 -0400

    Update route

--- src/routes.php
+++ src/routes.php
@@ -6,20 +6,37 @@ use Plume\Kernel\Router\Rute;
 $rute = new Rute;
 
 $rute->get('/', function () {
-    $content = '01.home';
+    /*$content = '01.home';
     $file = PATH_PAGES . "$content/index.txt";
-    echo file_get_contents($file);
+    echo file_get_contents($file);*/
+    return view('index', [
+        'title' => env('title') . ' - Home',
+        'message' => 'Bienvenidos a ' . env('title')
+    ]);
 });
 
 $rute->group('/admin', function ($rute) {
-        $rute->get('/', [Plume\Controller\Dashboard::class]);
+    $rute->map(['get','post'],'/', [Plume\Controller\Dashboard::class]);
+    $rute->get('/off',['Plume\Controller\Dashboard', 'off']);
+    
+    /*
+    $rute->get('/', [Plume\Controller\Dashboard::class]);
+    $rute->post('/', function () {
+        if ($_POST) {
+            return var_dump($_POST);
+        }
+    });
 
-        $rute->get('/user', [Plume\Controller\Dashboard::class, 'users']);
-        $rute->get('/off', [Plume\Controller\Dashboard::class, 'off']);
+    $rute->get('/user', [Plume\Controller\Dashboard::class, 'users']);
+    $rute->get('/off', [Plume\Controller\Dashboard::class, 'off']);*/
 });
 
 $rute->get('/(:any)', function () {
-    echo 'Pagina cualkiera';
+    //echo 'Pagina cualkiera';
+    return view('index', [
+        'title' => env('title') . ' - Another Pages',
+        'message' => '🙄<br>Esta es otra pagina'
+    ]);
 });
 
 $rute->routing();
